f0570e08909aca74bc9bda3523cb77ffa9ebed2a
Omri Sak <omri.sak@live.biu.ac.il>
2019-05-12 17:13:07 +0300

    created Z-score and different encoding function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import numpy as np
+from scipy import __all__ as sc
 
 
 def one_hot_encoding(data):
@@ -14,11 +15,27 @@ def one_hot_encoding(data):
         data[i][0] = encode[row[0]]
 
 
+def encoding(data):
+    encode = {
+        "M": 0,
+        "F": 1,
+        "I": 2
+    }
+    for i, row in enumerate(data):
+        # transform it into vector
+        data[i][0] = encode[row[0]]
+
+
+def z_score(data):
+    return sc.stats.mstats.zscore(data)
+
+
 def main():
     header = ['Sex', 'Length', 'Diameter', 'Height', 'W weight', 'S weight', 'V weight', 'Shell weight']
     df = pd.read_csv(sys.argv[1], names=header)  # df = data frame
     data_arr = np.array(df.iloc[:, 0:])  # return all the indexes, all rows from 0 to 3286 and columns from 0 to all
-    one_hot_encoding(data_arr)
+    encoding(data_arr)
+    data_arr = z_score(data_arr)
     print(data_arr)
 
 
