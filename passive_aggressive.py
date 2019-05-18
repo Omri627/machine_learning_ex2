0ff6049dfa279f24d0dfce2a1409b6b1b5242666
Omri Sak <omri.sak@live.biu.ac.il>
2019-05-18 16:32:52 +0300

    Merge branch 'master' of C:\Users\Omri\PycharmProjects\machine_learning_ex2 with conflicts.

--- passive_aggressive.py
+++ passive_aggressive.py
@@ -2,9 +2,9 @@ import numpy as np
 import random
 import math
 import tester
-import ex2
 
-def train(X, Y, samples_size):
+
+def train(X, Y):
     """
     passive aggressive algorithm is online algorithm used to form a linear classification/decision boundary
     between different classes. passive-aggressive is supervised learning algorithm which analyzes
@@ -14,13 +14,17 @@ def train(X, Y, samples_size):
     :param Y: collection of corresponding labels
     :return: returns a model which can be used for mapping new cases
     """
-    classes_size = 3                        # number of classes
-    features_size = len(X[0])               # quantity of features of each sample
-    indexes = np.arange(0, samples_size)    # indexes vector used to pick random examples from samples set
+    classes_size = 3  # number of classes
+    samples_size = len(X)  # quantity of samples in training set
+    features_size = len(X[0])  # quantity of features of each sample
+    indexes = np.arange(0, samples_size)  # indexes vector used to pick random examples from samples set
     # start with the all-zeroes weight matrix
     w = np.zeros((classes_size, features_size))
+    for i in range(0, classes_size):
+        for j in range(0, features_size):
+            w[i][j] = 0.1
     # the number of times to run through the training data while updating the weight.
-    epochs = 20
+    epochs = 10
     for e in range(0, epochs):
         # shuffle the data
         random.shuffle(indexes)
@@ -36,65 +40,46 @@ def train(X, Y, samples_size):
             if y_hat != y:
                 # compute tau: adaptive learning rate
                 norm_x = np.linalg.norm(X[i])
-                if norm_x == 0:
-                    tau = 0
-                else:
-                    loss = max(0, 1 - classes_value[y] + classes_value[y_hat])
-                    tau = loss / (2 * norm_x * norm_x)
+                loss = max(0, 1 - classes_value[y] + classes_value[y_hat])
+                tau = loss / (2 * norm_x * norm_x)
                 # applies multi-class passive-aggressive update rule
                 w[y_hat] = w[y_hat] - tau * X[i]
                 w[y] = w[y] + tau * X[i]
     # returns the model represented by weights matrix
     return w
 
-def predict(w, x):
-    """
-    predict method gets the model and example
-    and predicts his label using the model
 
-    :param w: model trained by passive-aggressive algorithm
-    :param x: example to predict his label
-    :return: returns the label of example predicted by model
-    """
-    return np.argmax(w, x)
+def test(w, X, Y):
+    err = 0  # error counter
+    samples_size = len(X)
+    for i in range(0, samples_size):
+        y_hat = np.argmax(np.dot(w, X[i]))
+        y = int(Y[i])
+        if y_hat != y:
+            err += 1
+    return float(err) / samples_size
+
+
+def predict(w, input):
+    return np.argmax(w, input)
 
 
 def norm(vector):
-    """
-    computes the norm of the given vector
-
-    :param vector: specific vector
-    :return: return real number specifies the norm of the vector
-    """
     sum = 0
-    # computes the summation of features squared of the vector
     for x in vector:
         sum = sum + (x * x)
     return math.sqrt(sum)
 
-def getBestModelPerShuffle(train_X, train_Y, test_X, test_Y):
-    """
-    getBestModelPerShuffle method trains fixed amount of models on training set using perceptron algorithm
-    for each model the method computes the error rate
-    and pick the best model i.e the model which gains the minimum error rate on validation set
 
-    :param train_X: set of examples in training set
-    :param train_Y: set of labels in training set
-    :param validation_X: set of examples of validation set
-    :param validation_Y: set of labels of validation set
-    :param samples_size number of samples in training set
-    :return: returns the best model which gains the minimum error rate
-    """
-    instances = 10              # number of models to train
-    min_error_rate = 1          # min error rate of models
-    min_model = []              # instance of model which obtained the min error rate
-    train_amount = len(train_X)  # amount of samples in training set
-    test_amount = len(test_X)  # amount of samples in test set
+def getBestModelPerShuffle(Train_X, Train_Y, Test_X, Test_Y):
+    instances = 20  # number of models to train
+    min_error_rate = 1  # min error rate of models
+    min_model = []  # instance of model which obtained the min error rate
     for i in range(0, instances):
         # builds new model by svm algorithm on training set
-        w = train(train_X, train_Y, train_amount)
+        w = train(Train_X, Train_Y)
         # computes the error rate of model on validation set
-        error_rate = tester.computeErrorRate(w, test_X, test_Y, test_amount)
+        error_rate = test(w, Test_X, Test_Y)
         # in case the mode achieved the min error rate, mark the model as the best
         if error_rate < min_error_rate:
             min_error_rate = error_rate
@@ -102,34 +87,17 @@ def getBestModelPerShuffle(train_X, train_Y, test_X, test_Y):
     # return the instance of model which obtained the min error rate
     return min_model, min_error_rate
 
-def getBestModel(train_X, train_Y, samples_size):
-    """
-    getBestModel method shuffles the training set and divides/splits the whole-training set
-    into two sets: train set and validation set.
-    for each shuffle the method runs getBestModelPerShuffle
-    that trains fixed amount of models and pick the model which gains the minimum error rate on validation set
-    the method, eventually choose the best model over the optimal models trained on different shuffles
 
-    :param train_X: set of examples of training set
-    :param train_Y: set of labels of training set
-    :param samples_size: the amount of samples on training set
-    :return: returns the best model over the optimal models trained on different shuffles
-    """
-    training_percentage = 0.80                          # percentage of samples used for training
-    shuffles_amount = 5                                 # amount of times of dividing the training set
-    min_model = []                                      # model which gains the min error rate
-    min_error_rate = 1                                  # minimum error rate of models
+def getBestModel(Train_X, Train_Y, samples_size):
+    shuffles_amount = 5
+    min_model = []
+    min_error_rate = 1
     for i in range(0, shuffles_amount):
-        # shuffle the set of samples
-        train_X, train_Y = ex2.unison_shuffled_copies(train_X, train_Y, samples_size)
-        # split the data and labels of given samples
-        split_data = np.split(train_X, [int(training_percentage * samples_size), samples_size])
-        split_label = np.split(train_Y, [int(training_percentage * samples_size), samples_size])
-        # get best model for this shuffle
+        Train_X, Train_Y = tester.unison_shuffled_copies(Train_X, Train_Y)
+        split_data = np.split(Train_X, [int(0.80 * samples_size), samples_size])
+        split_label = np.split(Train_Y, [int(0.80 * samples_size), samples_size])
         model, error_rate = getBestModelPerShuffle(split_data[0], split_label[0], split_data[1], split_label[1])
-        # in case this is the best model
         if error_rate < min_error_rate:
             min_error_rate = error_rate
             min_model = model
-    # return the min model i.e the model which gains the minimum error-rate
     return min_model
